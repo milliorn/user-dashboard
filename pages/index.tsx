15c7d9f9549e160bf7c128d0c5a8a11d65cf6760
milliorn <scottmilliorn@gmail.com>
2023-01-31 20:52:49 -0800

    index styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-5xl text-center">
+      <main className="bg-gray-100 min-h-screen">
         <Header />
       </main>
     </>
