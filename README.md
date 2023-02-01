17411e82f7c0ce9a598e386d0932507ca1f384e1
milliorn <scottmilliorn@gmail.com>
2023-01-31 20:55:55 -0800

    react icons

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ## Tailwind
 
 <https://tailwindcss.com/docs/guides/nextjs>
+
+## react-icons
+
+<https://www.npmjs.com/package/react-icons>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
-
 // global header component
 function Header(): JSX.Element {
   return (
-    <div>Header</div>
-  )
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dashboard</h2>
+      <h2>Welcome Back!</h2>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
