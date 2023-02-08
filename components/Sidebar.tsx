ab1ca7c59ada325481a9107de2e44ce0759ae55e
milliorn <scottmilliorn@gmail.com>
2023-02-07 18:33:04 -0800

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,21 +19,29 @@ function Sidebar({ children }: any): JSX.Element {
   return (
     <div className="flex">
       <div className="fixed w-20 h-full p-4 bg-white dark:bg-black dark:text-white border-r flex flex-col justify-between">
-        <div className="flex flex-col items-center">
-          <SidebarHome />
-          <span className="border-b border-gray-200 dark:border-gray-50 w-full p-4" />
-          {sidebarButtonsData.map((e, i) => (
-            <Link href={e.href} aria-label={e.label} key={i}>
-              <div className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-50 dark:hover:text-purple-800 cursor-pointer my-4 p-4 rounded-lg inline-block">
-                {e.icon}
-              </div>
-            </Link>
-          ))}
-        </div>
+        <SidebarButtons />
       </div>
       <main className="ml-20 w-full">{children}</main>
     </div>
   );
+
+  // sidebar buttons
+  function SidebarButtons() {
+    return (
+      <div className="flex flex-col items-center">
+        <SidebarHome />
+        <span className="border-b border-gray-200 dark:border-gray-50 w-full p-4" />
+
+        {sidebarButtonsData.map((e, i) => (
+          <Link href={e.href} aria-label={e.label} key={i}>
+            <div className="bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-50 dark:hover:text-purple-800 cursor-pointer my-4 p-4 rounded-lg inline-block">
+              {e.icon}
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  }
 
   // home button
   function SidebarHome() {
